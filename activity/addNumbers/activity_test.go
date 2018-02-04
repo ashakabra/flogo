e8d83275d4dc58ab5fd06a2203d41db97c8abd01
Asha Kabra <akabra@tibco.com>
2018-02-04 19:58:50 +0530

    Addition Numbers - Sum of two input integers

---
+++ activity/addNumbers/activity_test.go
@@ -0,0 +1,54 @@
+package addNumbers
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/TIBCOSoftware/flogo-contrib/action/flow/test"
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+)
+
+var activityMetadata *activity.Metadata
+
+func getActivityMetadata() *activity.Metadata {
+
+	if activityMetadata == nil {
+		jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
+		if err != nil {
+			panic("No Json Metadata found for activity.json path")
+		}
+
+		activityMetadata = activity.NewMetadata(string(jsonMetadataBytes))
+	}
+
+	return activityMetadata
+}
+
+func TestCreate(t *testing.T) {
+
+	act := NewActivity(getActivityMetadata())
+
+	if act == nil {
+		t.Error("Activity Not Created")
+		t.Fail()
+		return
+	}
+}
+
+func TestEval(t *testing.T) {
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	//setup attrs
+	tc.SetInput(ivNum1, 2)
+	tc.SetInput(ivNum2, 3)
+
+	act.Eval(tc)
+
+	sum := tc.GetOutput(ovAddition).(int)
+
+	if sum != 5 {
+		t.Error("Sum did not match")
+	}
+}
