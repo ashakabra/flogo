eedc18f33b6b8914c88785780af6401dac198490
Asha Kabra <akabra@tibco.com>
2018-04-18 14:24:59 +0530

    Simple Multiply activity

---
+++ activity/multiply/activity_test.go
@@ -0,0 +1,53 @@
+package multiply
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/TIBCOSoftware/flogo-contrib/action/flow/test"
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+)
+
+var activityMetadata *activity.Metadata
+
+func getActivityMetadata() *activity.Metadata {
+
+	if activityMetadata == nil {
+		jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
+		if err != nil {
+			panic("No Json Metadata found for activity.json path")
+		}
+
+		activityMetadata = activity.NewMetadata(string(jsonMetadataBytes))
+	}
+
+	return activityMetadata
+}
+
+func TestCreate(t *testing.T) {
+
+	act := NewActivity(getActivityMetadata())
+
+	if act == nil {
+		t.Error("Activity Not Created")
+		t.Fail()
+		return
+	}
+}
+
+func TestEval(t *testing.T) {
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	//setup attrs
+	tc.SetInput(ivNum1, "2")
+	tc.SetInput(ivNum2, "7")
+
+	act.Eval(tc)
+
+	mul := tc.GetOutput(ovMultiply).(int)
+	if mul != 14 {
+		t.Error("Multiply did not match")
+	}
+}
