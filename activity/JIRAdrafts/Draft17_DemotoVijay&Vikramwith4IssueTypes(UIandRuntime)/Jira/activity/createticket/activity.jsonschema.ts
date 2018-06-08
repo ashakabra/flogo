9cec7fc020771218471337bd378ecf339263e5ac
Asha Kabra <akabra@tibco.com>
2018-06-08 14:54:32 +0530

    JIRA 4 IssueTypes UI and Runtime-DemotoVijay&Vikram

---
+++ activity/JIRAdrafts/Draft17_DemotoVijay&Vikramwith4IssueTypes(UIandRuntime)/Jira/activity/createticket/activity.jsonschema.ts
@@ -0,0 +1,86 @@
+export module JsonSchema {
+
+    export const 
+        STRING = "string",
+        NUMBER = "number",        
+        ARRAY = "array";
+        
+    export class Types {
+
+        public static toJsonType(schema: any, fieldName: string): any {
+            let item = {};
+
+            let jiraDataType = "";
+            jiraDataType = schema.type;
+            
+            switch (jiraDataType) {
+                case "number":
+                    item = JsonSchema.Types.numberType(fieldName,jiraDataType);
+                    break;                
+                case "string":
+                case "option":
+                case "user":
+                case "date":
+                case "datetime":
+                case "project":
+                case "priority":
+                case "any":
+                    item = JsonSchema.Types.stringType(fieldName,jiraDataType);
+                    break;
+                case "array":
+                    item = JsonSchema.Types.arrayType(fieldName,schema);
+                    break;
+                default:
+                    console.log("Datatype case is not present :: "+jiraDataType)
+                    item = JsonSchema.Types.stringType(fieldName, jiraDataType);
+            }
+            return item;
+        }
+    
+        public static numberType(fieldName: string, jiraDatatype: string) {
+            return {
+                type: "number",
+                flogoJiraID : fieldName,
+                flogoJiraType : jiraDatatype
+            };
+        }
+    
+        public static stringType(fieldName: string, jiraDatatype: string) {
+            return {
+                type: "string",
+                flogoJiraID : fieldName,
+                flogoJiraType : jiraDatatype
+            };
+        }
+
+
+        public static arrayType(fieldName: string, schema: any) {
+            let arrayItems = schema.items , arrayType = "";
+            
+            switch (arrayItems){
+                case "version":
+                case "user":
+                case "component":
+                    arrayType = "ArrayOfName";
+                    break;
+
+                case "option":
+                    arrayType = "ArrayOfValue";
+                    break;
+
+                case "string":
+                    arrayType = "ArrayOfString";
+                    break;
+            }
+            
+            return {
+                "type": "array",
+                "items": {
+                  "type": "string"
+                },
+                flogoJiraID : fieldName,
+                flogoJiraType : arrayType
+              };
+        }        
+    }
+}
---
+++ activity/JIRAdrafts/Draft17_DemotoVijay&Vikramwith4IssueTypes(UIandRuntime)/Jira/activity/createticket/input.go
@@ -0,0 +1,75 @@
+package createticket
+
+import (
+	"encoding/json"
+
+	"github.com/TIBCOSoftware/flogo-lib/core/data"
+)
+
+func GetInputParameter(ivInput interface{}, dynamicMap map[string]interface{}, outputMap map[string]interface{}) {
+	activityLog.Info("Reading Input params")
+	complexInput := ivInput.(*data.ComplexObject)
+
+	metadata := make(map[string]interface{})
+	json.Unmarshal([]byte(complexInput.Metadata), &metadata)
+
+	props := metadata["properties"].(map[string]interface{})
+	inputValues := complexInput.Value.(map[string]interface{})
+
+	for key, value := range inputValues {
+		outputMap[key] = value
+		customFields := props[key].(map[string]interface{})
+		flogoJiraType := customFields["flogoJiraType"].(string)
+		flogoJiraID := customFields["flogoJiraID"].(string)
+
+		switch flogoJiraType {
+		case "option":
+			fieldValue := make(map[string]interface{})
+			fieldValue["value"] = value
+			dynamicMap[flogoJiraID] = fieldValue
+
+		case "user", "priority":
+			fieldValue := make(map[string]interface{})
+			fieldValue["name"] = value
+			dynamicMap[flogoJiraID] = fieldValue
+
+		case "project":
+			fieldValue := make(map[string]interface{})
+			fieldValue["key"] = value
+			dynamicMap[flogoJiraID] = fieldValue
+
+		case "ArrayOfName":
+			var arrayOfName []interface{}
+			for _, v1 := range value.([]interface{}) {
+				fieldValue := make(map[string]interface{})
+				fieldValue["name"] = v1
+				arrayOfName = append(arrayOfName, fieldValue)
+			}
+			dynamicMap[flogoJiraID] = arrayOfName
+
+		case "ArrayOfValue":
+			var arrayOfValue []interface{}
+			for _, v1 := range value.([]interface{}) {
+				fieldValue := make(map[string]interface{})
+				fieldValue["value"] = v1
+				arrayOfValue = append(arrayOfValue, fieldValue)
+			}
+			dynamicMap[flogoJiraID] = arrayOfValue
+
+		case "ArrayOfString":
+			var arrayOfString []string
+			for _, v1 := range value.([]interface{}) {
+				arrayOfString = append(arrayOfString, v1.(string))
+			}
+			dynamicMap[flogoJiraID] = arrayOfString
+
+		case "string", "number", "date", "any":
+			dynamicMap[flogoJiraID] = value
+
+		case "default":
+			activityLog.Infof("JIRA datatype that is not handled by code :: %s ", flogoJiraType)
+		}
+
+	}
+
+}
